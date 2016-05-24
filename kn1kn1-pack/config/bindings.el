79a8c8f0a3afd94d766f043036a53e057b63ec26
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-05-24 16:28:07 +0900

    Add WORKAROUND for paredit-mode ctr - <- and ctr - -> does not properly work with tmux

--- kn1kn1-pack/config/bindings.el
+++ kn1kn1-pack/config/bindings.el
@@ -3,3 +3,7 @@
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; for tmux/mosh
+(define-key paredit-mode-map (kbd "M-[ c") 'paredit-forward-slurp-sexp)
+(define-key paredit-mode-map (kbd "M-[ d") 'paredit-forward-barf-sexp)
